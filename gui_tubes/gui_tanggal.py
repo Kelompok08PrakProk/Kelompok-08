e8de9404a86d3b48d7a9be8969efb6870b802ee0
Adelia Saqina <adeliasaqina18@gmail.com>
2024-05-29 00:48:11 +0700

    PETI

--- gui_tubes/gui_tanggal.py
+++ gui_tubes/gui_tanggal.py
@@ -52,16 +52,14 @@ logo2_path = os.path.join(script_dir, "gambar/logo2.png")
 icon_search_path = os.path.join(script_dir, "gambar/search_icon.png")
 icon_acc_path = os.path.join(script_dir, "gambar/account_icon.png")
 cover_buku_path = os.path.join(script_dir, "gambar/cover buku/Novel/1.jpg")  # Path ke gambar sampul buku
+background_cover_path = os.path.join(script_dir, "gambar/Background/Novel/Laut Bercerita.jpg")
 
 # Memuat dan mengubah ukuran gambar logo
 logo2 = ctk.CTkImage(light_image=Image.open(logo2_path), size=(320, 117))
 icon_search = ctk.CTkImage(light_image=Image.open(icon_search_path), size=(20, 20))
 icon_acc = ctk.CTkImage(light_image=Image.open(icon_acc_path), size=(70, 70))
 cover_buku = ctk.CTkImage(light_image=Image.open(cover_buku_path), size=(200, 300))
-
-logo2 = ctk.CTkImage(light_image=Image.open(logo2_path), size=(320, 117))
-icon_search = ctk.CTkImage(light_image=Image.open(icon_search_path), size=(20, 20))
-icon_acc = ctk.CTkImage(light_image=Image.open(icon_acc_path), size=(70, 70))
+background_cover= ctk.CTkImage(light_image=Image.open(background_cover_path), size=(1850,200))
 
 # Memuat gambar sampul buku
 cover_buku = ctk.CTkImage(light_image=Image.open(cover_buku_path), size=(250, 350))
@@ -69,6 +67,10 @@ cover_buku = ctk.CTkImage(light_image=Image.open(cover_buku_path), size=(250, 35
 # Membuat label untuk logo
 l1 = ctk.CTkLabel(window_beranda, image=logo2, text="", bg_color="transparent", fg_color="transparent")
 l1.pack(pady=0, anchor="nw")
+
+# Background covernya
+background_cover = ctk.CTkLabel(window_beranda, image=background_cover, text="", bg_color="transparent", fg_color="transparent")
+background_cover.place(relx=0.5, rely=0.32, anchor="center")
 
 # Membuat tombol "Beranda"
 button1 = ctk.CTkButton(window_beranda, width=120, height=35, corner_radius=30, fg_color="#1A1F23", border_width=1, border_color="#A84F6C", text="Beranda", text_color="#E3DFE6", font=("Trebuchet MS", 16), hover=True, hover_color="#A84F6C")
@@ -92,33 +94,33 @@ button4.place(relx=0.95, rely=0.055, anchor="center")
 
 # Membuat tombol "Kembali"
 button_back = ctk.CTkButton(window_beranda, width=100, height=35, corner_radius=0, fg_color="#1A1F23", border_width=0, border_color="#A84F6C", text="← Kembali", text_color="#E3DFE6", font=("Trebuchet MS", 16), hover=True, hover_color="#A84F6C", command=kembali)
-button_back.place(relx=0.08, rely=0.2, anchor="w")
+button_back.place(relx=0.08, rely=0.15, anchor="w")
 
 frame_tanggal_pinjam = ctk.CTkFrame(window_beranda, width=500, height=60, corner_radius=10, fg_color="#e3dfe6")
-frame_tanggal_pinjam.place(relx=0.3, rely=0.4, anchor="center")
+frame_tanggal_pinjam.place(relx=0.3, rely=0.55, anchor="center")
 
 # Membuat tombol "Tanggal Peminjaman"
 button_tanggal_pinjam = ctk.CTkButton(window_beranda, width=250, height=40, corner_radius=10, bg_color= "#e3dfe6", fg_color="#a6a4a8", border_width=1, border_color="#a6a4a8", text="Tanggal Peminjaman", text_color="#000000", font=("Trebuchet MS", 16), hover=True, hover_color="#AAAAAA")
-button_tanggal_pinjam.place(relx=0.23, rely=0.4, anchor="center")
+button_tanggal_pinjam.place(relx=0.23, rely=0.55, anchor="center")
 button_tanggal_pinjam.configure(command=lambda: show_calendar(button_tanggal_pinjam))  # Adjusted here
 
 # Tombol untuk menampilkan tanggal yang telah dipilih
 button_tampilkan_pinjam = ctk.CTkButton(window_beranda, width=150, height=40, corner_radius=10, bg_color= "#e3dfe6", fg_color="#e3dfe6", border_width=1, border_color="#e3dfe6", text="", text_color="#000000", font=("Trebuchet MS", 12), hover=False, hover_color="#AAAAAA")
-button_tampilkan_pinjam.place(relx=0.4, rely=0.4, anchor="center")
+button_tampilkan_pinjam.place(relx=0.4, rely=0.55, anchor="center")
 button_tanggal_pinjam.configure(command=lambda: show_calendar(button_tanggal_pinjam))
 
 
 frame_tanggal_kembali = ctk.CTkFrame(window_beranda, width=500, height=60, corner_radius=10, fg_color="#e3dfe6")
-frame_tanggal_kembali.place(relx=0.3, rely=0.55, anchor="center")
+frame_tanggal_kembali.place(relx=0.3, rely=0.65, anchor="center")
 
 # Membuat tombol "Tanggal Pengembalian"
 button_tanggal_kembali = ctk.CTkButton(window_beranda, width=250, height=40, corner_radius=10, bg_color= "#e3dfe6", fg_color="#a6a4a8", border_width=1, border_color="#a6a4a8", text="Tanggal Pengembalian", text_color="#000000", font=("Trebuchet MS", 16), hover=True, hover_color="#AAAAAA")
-button_tanggal_kembali.place(relx=0.23, rely=0.55, anchor="center")
+button_tanggal_kembali.place(relx=0.23, rely=0.65, anchor="center")
 button_tanggal_kembali.configure(command=lambda: show_calendar(button_tanggal_kembali))  # Adjusted here
 
 # Tombol untuk menampilkan tanggal yang telah dipilih
 button_tampilkan_kembali = ctk.CTkButton(window_beranda, width=150, height=40, corner_radius=10, bg_color= "#e3dfe6", fg_color="#e3dfe6", border_width=1, border_color="#e3dfe6", text="", text_color="#000000", font=("Trebuchet MS", 12), hover=False, hover_color="#AAAAAA")
-button_tampilkan_kembali.place(relx=0.4, rely=0.55, anchor="center")
+button_tampilkan_kembali.place(relx=0.4, rely=0.65, anchor="center")
 button_tanggal_kembali.configure(command=lambda: show_calendar(button_tanggal_kembali))
 
 # Membuat label untuk menampilkan tanggal pengembalian
