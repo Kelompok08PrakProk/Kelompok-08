3dc4ddfdc852768d12947d77b5b2bb27dc56aa15
Dimas Adira Sugiyanto <dimsforcollege@student.uns.ac.id>
2024-05-30 02:37:36 +0700

    PETI

--- gui_beranda.py
+++ gui_beranda.py
@@ -17,17 +17,14 @@ def tampilkan_daftar_buku(scrollable_frame, file_path='database/databuku.csv'):
         # Membaca file CSV ke dalam DataFrame
         df = pd.read_csv(file_path, encoding="windows-1252")
         
-        # Memeriksa apakah kolom 'judul' ada dalam DataFrame
-        if 'judul' not in df.columns:
-            print("Kolom 'judul' tidak ditemukan dalam file CSV.")
+        # Memeriksa apakah kolom 'judul' dan 'cover' ada dalam DataFrame
+        if 'judul' not in df.columns or 'cover' not in df.columns:
+            print("Kolom 'judul' atau 'cover' tidak ditemukan dalam file CSV.")
             return
-        
-        # Load cover buku dari direktori coverbuku
-        cover_dir = os.path.join(script_dir, 'cover buku')
         
         # Menampilkan daftar buku
         for idx, row in df.iterrows():
-            cover_path = os.path.join(cover_dir, f"{row['judul']}.jpeg")
+            cover_path = row['cover']
             
             if os.path.exists(cover_path):
                 try:
@@ -40,13 +37,13 @@ def tampilkan_daftar_buku(scrollable_frame, file_path='database/databuku.csv'):
             else:
                 print(f"Cover image not found: {cover_path}")
                 cover_photo = ImageTk.PhotoImage(Image.new("RGB", (300, 450), color="gray"))  # Placeholder abu-abu jika gambar tidak ditemukan
-
             frame_buku = ctk.CTkFrame(scrollable_frame, width=360, height=600, fg_color="#1A1F23")
             frame_buku.pack(side="left", padx=10, pady=10)
 
-            label_cover = ctk.CTkLabel(frame_buku, image=cover_photo, text="", fg_color="transparent")
-            label_cover.image = cover_photo  # Menyimpan referensi gambar
-            label_cover.pack(side="top")
+            # Change label_cover to a button
+            button_cover = ctk.CTkButton(frame_buku, image=cover_photo, text="", fg_color="transparent", command=lambda judul=row['judul'], genre=row['genre'], penulis=row['penulis'], tahun_terbit=row['tahunTerbit'], halaman=row['halaman'], sinopsis=row['sinopsis'], stok=row['stok']: tampilkan_detail_buku(judul, genre, penulis, tahun_terbit, halaman, sinopsis, stok))
+            button_cover.image = cover_photo  # Menyimpan referensi gambar
+            button_cover.pack(side="top")
 
             label_judul = ctk.CTkLabel(frame_buku, text=row['judul'], fg_color="transparent", text_color="#E3DFE6")
             label_judul.pack(side="top")
@@ -111,49 +108,6 @@ def tampilkan_buku_berdasarkan_genre(category_scrollable_frame, genre, file_path
     except Exception as e:
         print(f"Terjadi kesalahan: {e}")
 
-def show_novel_details(search_results_frame,novel_image_path, novel_description):
-    try:
-        novel_image = Image.open(novel_image_path)
-        resize_novel_img = novel_image.resize((350, 450))
-        novel_image = ImageTk.PhotoImage(resize_novel_img)
-        novel_image_path = (os.path.join(script_dir, "gambar/cover buku/Novel/1.jpg"))
-        novel_description = (os.path.join(script_dir,"Tahun Terbit: 2017\nJumlah Halaman: 379\n\nSinopsis : Laut Bercerita, novel terbaru Leila S. Chudori, bertutur tentang kisah keluarga yang kehilangan, sekumpulan sahabat yang merasakan kekosongan di dada, sekelompok orang yang gemar menyiksa dan lancar berkhianat, sejumlah keluarga yang mencari kejelasan makam anaknya, dan tentang cinta yang tak akan luntur."))
-
-        novel_img_label = ctk.CTkLabel(search_results_frame, image=novel_image, text="", bg_color="transparent", fg_color="transparent")
-        novel_img_label.image = novel_image  
-        novel_img_label.place(relx=0.25, rely=0.5, anchor="center")
-        
-        novel_desc_text = Text(search_results_frame, wrap='word', bg='#1A1F23', fg='#E3DFE6', font=("Trebuchet MS", 16), borderwidth=0, highlightthickness=0)
-        novel_desc_text.insert("1.0", novel_description)
-        novel_desc_text.tag_add("justify", "1.0", "end")
-        novel_desc_text.tag_configure("justify", justify="left")
-        novel_desc_text.place(relx=0.51, rely=0.6, anchor="center", width=600, height=400)
-        novel_desc_text.configure(state="disabled")
-
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        back_arrow_path = os.path.join(script_dir, "gambar/back_arrow.png")
-        back_arrow = Image.open(back_arrow_path)
-        resize_arrow_img = back_arrow.resize((200, 25))
-        back_arrow = ImageTk.PhotoImage(resize_arrow_img)
-
-        back_button = ctk.CTkButton(search_results_frame, image=back_arrow, text="Kembali", width=40, height=40, corner_radius=30, fg_color="transparent" , command=back_to_home)
-        back_button.image = back_arrow  
-        back_button.place(relx=0.05, rely=0.11, anchor="nw")
-
-        stock_button = ctk.CTkButton(search_results_frame, text="Stok : 10", width=50, height=25, corner_radius=30, fg_color="#A84F6C", border_width=1, border_color="#A84F6C", text_color="#E3DFE6", font=("Trebuchet MS", 16))
-        stock_button.place(relx=0.39, rely=0.7, anchor="center")
-        
-        borrow_button = ctk.CTkButton(search_results_frame, text="PINJAM", width=120, height=35, corner_radius=30, fg_color="transparent", border_width=1, border_color="#A84F6C", text_color="#E3DFE6", font=("Trebuchet MS", 16))
-        borrow_button.place(relx=0.4, rely=0.8, anchor="center")
-        
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
-
-def back_to_home():
-    for widget in window_beranda.winfo_children():
-        widget.destroy()
-    setup_home_screen()
-
 def setup_home_screen():
     logo2_path = os.path.join(script_dir, "gambar/logo2.png")
     icon_search_path = os.path.join(script_dir, "gambar/search_icon.png")
@@ -180,12 +134,6 @@ def setup_home_screen():
         category_frame.pack_forget()
         main_frame.pack(side=ctk.TOP, fill="both", expand=True)
         tampilkan_daftar_buku(scrollable_frame)
-    
-    def show_result():
-        main_frame.pack_forget()
-        category_frame.pack_forget()
-        search_results_frame.pack(side=ctk.TOP, fill="both", expand=True)
-        show_novel_details(search_results_frame)
 
     def show_kategori():
         indicate(button2_indicate)
@@ -259,7 +207,7 @@ def setup_home_screen():
     button2_indicate.place(relx=0.63, rely=0.4, anchor="center")
 
     button3 = ctk.CTkButton(window_beranda, width=20, height=20, image=icon_search, corner_radius=0, bg_color="#9C909D", 
-                            fg_color="#9C909D", border_width=0, text="", hover=False, command=show_result)
+                            fg_color="#9C909D", border_width=0, text="", hover=False)
     button3.place(relx=0.88, rely=0.055, anchor="center")
 
     button4 = ctk.CTkButton(window_beranda, width=6, height=6, image=icon_acc, corner_radius=0, bg_color="#1A1F23", 
@@ -274,8 +222,6 @@ def setup_home_screen():
 
     category_scrollable_frame = ctk.CTkScrollableFrame(category_frame, width=1200, height=500, fg_color="#1A1F23", orientation="horizontal")
     category_scrollable_frame.pack(side="top", fill="both", expand=True)
-
-    show_result()
 
     show_beranda()
     
